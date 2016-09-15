6f22d5e9a6169dcc8edd68ac03065ca2ca300e5f
David Lewitan <dovidlwtn@gmail.com>
2016-09-15 21:16:30 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,8 @@
 # Write your #display_rainbow method here
+def display_rainbow(arr)
+  str = ""
+  (arr.length).times do |x|
+    str += "#{arr[x][0].upcase}: #{arr[x]}#{x != 6 ? ', ' : ''}"
+  end
+  puts str
+end
