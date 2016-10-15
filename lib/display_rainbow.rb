e057939abc9afba61741b8656eecbd6e450c4723
Chenxi Wu <wuchenxi2013@gmail.com>
2016-10-15 12:12:16 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,9 @@
 # Write your #display_rainbow method here
+def display_rainbow(rc)
+  def strgen(d,r)
+  cur=(r[d].ord-'a'.ord+'A'.ord).chr+": #{r[d]}"
+  (r.length-1==d)?cur:cur+", "+strgen(d+1,r)
+  end
+  puts strgen(0,rc)
+end
+#display_rainbow(["a","b"])
