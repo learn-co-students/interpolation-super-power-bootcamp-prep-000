7211200eae26e9afcfe38ce0f06e69e6e2212a62
Dequan Xiao <splscoupon@gmail.com>
2016-10-17 23:32:44 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,9 @@
 # Write your #display_rainbow method here
+
+def color_printer(color)
+  return "#{color[0].upcase}: #{color}"
+end
+
+def display_rainbow(colors)
+  puts "#{color_printer(colors[0])}, #{color_printer(colors[1])}, #{color_printer(colors[2])}, #{color_printer(colors[3])}, #{color_printer(colors[4])}, #{color_printer(colors[5])}, #{color_printer(colors[6])}"
+end
