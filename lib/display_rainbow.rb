d91e44b8c0fa2faf9d861c299c75457f59e1e58a
Rathi Nair-Das <rathind14@gmail.com>
2016-11-07 14:15:55 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,4 @@
-# Write your #display_rainbow method here
+def display_rainbow(arr)
+  colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+ return  puts ("R: #{arr[0]}, O: #{arr[1]}, Y: #{arr[2]}, G: #{arr[3]}, B: #{arr[4]}, I: #{arr[5]}, V: #{arr[6]}")if arr ==colors 
+end
