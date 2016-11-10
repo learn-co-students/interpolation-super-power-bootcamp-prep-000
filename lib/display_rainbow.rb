4508757aaefbe6804c679ae9b937cbf81c39bb5a
Ethan Ryan <ethanryan@gmail.com>
2016-11-10 22:57:55 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,25 +1,9 @@
 # Write your #display_rainbow method here
 
-# colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 
-
-
-
-# def display_rainbow(colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
 def display_rainbow(colors)
-
-  # puts "R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"
-  puts "R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"
-  # puts "The first color in the rainbow is: #{colors[0]}"
-  # puts "The second color in the rainbow is: #{colors[1]}"
-  # puts "The third color in the rainbow is: #{colors[2]}"
-  # puts "The forth color in the rainbow is: #{colors[3]}"
-  # puts "The fifth color in the rainbow is: #{colors[4]}"
-  # puts "The sixth color in the rainbow is: #{colors[5]}"
-  # puts "The seventh color in the rainbow is: #{colors[6]}"
+  puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
 end
 
-# display_rainbow(red, orange, yellow, green, blue, indigo, violet)
-
-# display_rainbow(colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"])
 display_rainbow(colors)
