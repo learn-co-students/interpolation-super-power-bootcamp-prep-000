2d46a83b07e8d20b70ecb693fc45d687ec969657
Daniel Seehausen <daniel.seehausen@gmail.com>
2016-11-13 00:17:30 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,15 @@
 # Write your #display_rainbow method here
+def display_rainbow(colors)
+    str = ""
+    k = ["R: ", "O: ", "Y: ", "G: ", "B: ", "I: ", "V: "]
+    idx = 0
+    k.each do |ch|
+        if idx == 6
+            str << (ch + colors[idx])
+        else
+            str << (ch + colors[idx] + ", ")
+        idx += 1
+        end
+    end
+    puts str
+end
