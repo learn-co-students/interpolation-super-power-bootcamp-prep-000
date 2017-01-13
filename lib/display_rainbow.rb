4f2b8454bb3644b824b8bef550db3c7c4527e18b
Megan McLean <mmclean@protonmail.com>
2017-01-13 06:46:29 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,9 @@
-# Write your #display_rainbow method here
+# Learn.co Bootcamp Prep - Ruby Fundamentals - 09: Interpolation Super Power
+
+# Define a method, '#display_rainbow', that accepts one parameter(s):
+# 'colors', an array of colours.
+# print the colors of the rainbow.
+
+def display_rainbow(colors)
+  puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
+end
