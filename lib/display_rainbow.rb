712f1b208a7fb88adec98827d13d10af71fd5eeb
William Barela <william.barela.dev@gmail.com>
2017-02-23 06:13:28 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,26 @@
 # Write your #display_rainbow method here
+#colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+
+def display_rainbow(colors)
+  output_string = ""
+  for i in 0..(colors.length - 1)
+    output_string += "#{colors[i][0].upcase}: #{colors[i]}, "
+  end
+  output_string = output_string[0..-3]
+  puts output_string
+end
+
+
+# a better way of making this method
+=begin
+def display_rainbow(colors)
+  output_string = ""
+
+  for color in colors
+    output_string += "#{color[0].upcase}: #{color}, "
+  end
+
+  output_string = output_string[0..-3]
+  puts output_string
+end
+=end
