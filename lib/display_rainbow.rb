e7c5cb47e8c36854c4039c64b9fbd8f9563c9308
Charles Jarju <charlesjarju@gmail.com>
2017-03-08 17:29:20 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,6 @@
 # Write your #display_rainbow method here
+def display_rainbow(arr)
+  outs = "#{arr[0][0].upcase}: #{arr[0]}, #{arr[1][0].upcase}: #{arr[1]}, #{arr[2][0].upcase}: #{arr[2]}, #{arr[3][0].upcase}: #{arr[3]}, #{arr[4][0].upcase}: #{arr[4]}, "
+  outs += "#{arr[5][0].upcase}: #{arr[5]}, #{arr[6][0].upcase}: #{arr[6]}"
+  puts outs
+end
