73f665bcb8dd259e0bc16713b7a688ea4de09366
Niwei Cai <niwei.cai@outlook.com>
2017-03-16 00:54:27 +0000

    Done.

--- spec/display_rainbow_spec.rb
+++ spec/display_rainbow_spec.rb
@@ -22,3 +22,9 @@ describe '#display_rainbow' do
     expect { display_rainbow(colors) }.to output("R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet\n").to_stdout
   end
 end
+
+colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+
+def display_rainbow(colors)
+  puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}\n".to_s
+end
