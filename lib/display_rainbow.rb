c4a0287f13aeeae72a8e88f2b8642d2015f09758
lita stephenson <lita.stephenson@gmail.com>
2017-06-18 03:22:23 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,10 @@
 # Write your #display_rainbow method here
+def display_rainbow(array)
+  #puts "#{array[0][0].upcase}: #{array[0]}, #{array[1][0].upcase}: #{array[1]}, #{array[2][0].upcase}: #{array[2]}, #{array[3][0].upcase}: #{array[3]}, #{array[4][0].upcase}: #{array[4]}, #{array[5][0].upcase}: #{array[5]}, #{array[6][0].upcase}: #{array[6]}"
+
+  array.each_with_index do |el, idx|
+    print "#{array[idx][0].upcase}: #{array[idx]}"
+    print ", " unless idx == array.length - 1
+  end
+  print "\n"
+end
