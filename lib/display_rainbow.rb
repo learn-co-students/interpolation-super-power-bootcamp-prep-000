91c227a50be9b491f2f7aebd050060be9e2a7cc9
Jessica Holliday <hazenbrassy@gmail.com>
2017-06-25 03:37:40 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,9 @@
 # Write your #display_rainbow method here
+def display_rainbow(rnb)
+  print "R: #{rnb[0]}, O: #{rnb[1]}, "
+  print "Y: #{rnb[2]}, "
+  print "G: #{rnb[3]}, "
+  print "B: #{rnb[4]}, "
+  print "I: #{rnb[5]}, "
+  puts "V: #{rnb[6]}"
+end
