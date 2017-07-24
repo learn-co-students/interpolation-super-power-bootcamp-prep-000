fc5467e7fe1a5716d774e850a598c9aae3150242
Rob Baranowski <robbaranowski@gmail.com>
2017-07-24 16:19:54 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,11 @@
 # Write your #display_rainbow method here
+def display_rainbow(colors)
+  s = ""
+  colors.each do |color|
+    s += "#{color[0].upcase}: #{color}, "
+  end
+  #remove last comma and space
+  puts s.chomp(', ')
+end
+
+#display_rainbow(['red', 'orange'])
