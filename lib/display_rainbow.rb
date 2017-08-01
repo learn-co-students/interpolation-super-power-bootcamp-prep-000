1876876795bc04bdd83d21c5632930d6b616fb6b
Michael Roose <mjroose@gmail.com>
2017-08-01 01:01:36 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,13 @@
 # Write your #display_rainbow method here
+
+def display_rainbow(colors)
+  initials = ["R", "O", "Y", "G", "B", "I", "V"]
+  str = "#{initials[0]}: #{colors[0]}"
+  str += ", #{initials[1]}: #{colors[1]}"
+  str += ", #{initials[2]}: #{colors[2]}"
+  str += ", #{initials[3]}: #{colors[3]}"
+  str += ", #{initials[4]}: #{colors[4]}"
+  str += ", #{initials[5]}: #{colors[5]}"
+  str += ", #{initials[6]}: #{colors[6]}"
+  puts str
+end
