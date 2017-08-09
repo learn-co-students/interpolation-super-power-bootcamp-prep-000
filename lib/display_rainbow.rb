94d931185ee5b13b8f50cc41ba087f85d6a1f1e7
William Scripps <wlscripps@gmail.com>
2017-08-09 03:25:05 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,8 @@
 # Write your #display_rainbow method here
+def display_rainbow(color)
+    rain = ""
+    for i in color
+    rain += i[0].capitalize + ": " + i + ", "
+  end
+puts rain.chomp(", ")
+end
