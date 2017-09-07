8d94eed68f06fa1c6bd2698acb0db84eb45f600d
KevinUndaho <mariahmurray5@gmail.com>
2017-09-07 18:56:17 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,26 @@
 # Write your #display_rainbow method here
+def display_rainbow(colors)
+  output_string = ""
+  i = 0
+  loop do
+    new_bit = "#{colors[i][0].upcase}: #{colors[i]}"
+    #puts new_bit
+    output_string += new_bit
+    #puts output_string
+
+    i+=1
+    #puts i
+    if i >= colors.length
+      #puts "break"
+      break
+    end
+    output_string += ", "
+  end
+
+  puts output_string
+
+end
+
+# colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+
+# display_rainbow(colors)
