e7bc9576539bc291c30b478536bf29fa8f2fa3d5
HumzahChoudry <humzah.choudry@gmail.com>
2017-09-12 01:50:54 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,14 @@
 # Write your #display_rainbow method here
+def display_rainbow(array)
+  # i = 0
+  # while(i < array.length){
+  #   firstLetter = array[i][0].uppercase
+  #   if(i < array.length - 1){
+  #     print(firstLetter + ": #{array[i]}, ")
+  #   }
+  # else
+  #   puts(firstLetter + ": #{array[i]}")
+  # }
+puts "R: #{array[0]}, O: #{array[1]}, Y: #{array[2]}, G: #{array[3]}, B: #{array[4]}, I: #{array[5]}, V: #{array[6]}"
+  
+end
