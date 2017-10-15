dfebf1d1ef8e924fa999810bc1e1895ddb8cc987
Andrew Siplas <andrew@asiplas.net>
2017-10-15 07:50:49 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,10 @@
-# Write your #display_rainbow method here
+def display_rainbow(colors)
+  retval = []
+  colors.each_with_index do |color, i|
+    colors[i] // aargh!
+    retval << "#{color[0].upcase}: #{color}"
+  end
+  puts retval.join(', ')
+end
+
+display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
