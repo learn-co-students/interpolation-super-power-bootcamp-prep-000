f8f2d53648cba4372b96c9a8193370967aae0f67
mrgunter51 <mrgunter@crimson.ua.edu>
2017-10-24 21:11:41 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -7,5 +7,16 @@ def display_rainbow(colors)
   puts "#{colors[0][0].upcase}: #{colors[0]}, #{colors[1][0].upcase}: #{colors[1]}, #{colors[2][0].upcase}: #{colors[2]}, #{colors[3][0].upcase}: #{colors[3]}, #{colors[4][0].upcase}: #{colors[4]}, #{colors[5][0].upcase}: #{colors[5]}, #{colors[6][0].upcase}: #{colors[6]}"
 end
 
+# def display_rainbow(colors)
+#   colors.each do |x|
+#     print "#{x[0].upcase}: #{x}"
+#     if(x != colors[colors.length - 1])
+#       print ", "
+#     else
+#       print "\n"
+#     end
+#   end
+# end
+
 colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 display_rainbow(colors)
