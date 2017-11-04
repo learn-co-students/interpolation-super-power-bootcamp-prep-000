e69f6c41804f5dc287644d4e527adb6fd54e24ac
Kevin Randles <krandles@gmail.com>
2017-11-04 02:16:15 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,10 @@
 # Write your #display_rainbow method here
 def display_rainbow(colors)
-  puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
+  result = ""
+  colors.each do |color|
+    result += "#{color[0].upcase}: #{color}, "
+  end
+  result = result.chomp.chop
+  puts result.chop
 end
+display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
