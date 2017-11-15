e4c8983234d414df4980571c09f6cf8634274fa7
Yufeng Liu Reach <yufeng8@vt.edu>
2017-11-15 22:21:34 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,13 @@
 # Write your #display_rainbow method here
+def display_rainbow(colors)
+  s=""
+  n=colors.length
+  limit = n-2
+  for i in 0..limit
+    s1= colors[i][0].upcase
+    s = s + "#{s1}: #{colors[i]}, "
+  end
+  s2= colors[n-1][0].upcase
+  s = s + "#{s2}: #{colors[n-1]}"
+  puts s
+end
