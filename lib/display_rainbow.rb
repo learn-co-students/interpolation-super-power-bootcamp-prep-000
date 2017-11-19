15fa508e1895d5c53549500277f3ae8f6160cb71
James Gary <bucket@earthlink.net>
2017-11-19 18:51:25 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,4 @@
-# Write your #display_rainbow method here
+def display_rainbow (col_arr)
+  str_out = "R: #{col_arr[0]}, O: #{col_arr[1]}, Y: #{col_arr[2]}, G: #{col_arr[3]}, B: #{col_arr[4]}, I: #{col_arr[5]}, V: #{col_arr[6]}"
+  puts str_out
+end
