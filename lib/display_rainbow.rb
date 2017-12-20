7a6ea5c5fb69c45cbfc5ccce3f30873fcddcfdc5
Stephane <son.trinh.dev@gmail.com>
2017-12-20 15:53:07 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,12 @@
 # Write your #display_rainbow method here
+
+def display_rainbow(colors)
+  string = ''
+  colors.each_with_index do |color, position|
+    string += "#{color[0].capitalize}: #{color}"
+    if position < colors.size - 1
+      string += ", "
+    end
+  end
+  print string
+end
