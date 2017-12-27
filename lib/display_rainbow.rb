c2b95454e52d67d6520f9c41d311ea5e5fcf7913
Jeremy Gottfried <jeremyagottfried@gmail.com>
2017-12-27 23:55:10 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,20 @@
 # Write your #display_rainbow method here
+bob     = ["Bob", 46, "Juggler", "555-555-5555"]
+stefani = ["Stefani", 49, "Firefighter", "555-111-1111"]
+def print_business_card(person)
+  puts "Name: #{person[0]}, Age: #{person[1]}, Occupation: #{person[2]}"
+  puts "Contact: #{person[3]}"
+end
+print_business_card(bob)
+ 
+# => "Name: Bob, Age: 46, Occupation: Juggler"
+# => "Contact: 555-555-5555"
+ 
+print_business_card(stefani)
+ 
+# => "Name: Stefani, Age: 49, Occupation: Firefighter"
+# => "Contact: 555-111-1111"
+
+def display_rainbow(colors) 
+  puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
+end
