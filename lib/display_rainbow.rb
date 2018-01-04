544a4af5a84063c92692f71a31dc570e9f4cc5eb
lsiler23 <siler.laurette@gmail.com>
2018-01-04 17:11:35 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,13 @@
 # Write your #display_rainbow method here
+def display_rainbow(colors)
+  # result = Array.new 
+  
+  # colors.each do |clr|
+  #   result << "#{clr[0].upcase}: #{clr}"
+  # end 
+  
+  # print result.join(", ").strip 
+  
+  puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
+
+end 
