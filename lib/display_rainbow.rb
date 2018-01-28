dbea8bd69a328b892f278f3f3d60400b42bcbb0b
Micah Shute <micah.shute@gmail.com>
2018-01-28 04:20:05 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,14 @@
-# Write your #display_rainbow method here
+def display_rainbow(colors)
+  titles = ["R","O","Y","G","B","I","V"]
+  string = ""
+  for i in 0..(titles.length()-1) do
+    string << "#{titles[i]}: #{colors[i]}"
+    if i != (titles.length() -1)
+      string << ", "
+    end
+  end
+  puts string
+end
+
+
+      
