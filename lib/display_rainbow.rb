4025826a66f4015e5a9459d4e8bb292de6acfd2b
Yung Leung <yungl.leung1@gmail.com>
2018-02-02 15:55:02 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,4 @@
 # Write your #display_rainbow method here
+def display_rainbow (arrayOfColors)
+  puts "R: #{arrayOfColors[0]}, O: #{arrayOfColors[1]}, Y: #{arrayOfColors[2]}, G: #{arrayOfColors[3]}, B: #{arrayOfColors[4]}, I: #{arrayOfColors[5]}, V: #{arrayOfColors[6]}"
+end
