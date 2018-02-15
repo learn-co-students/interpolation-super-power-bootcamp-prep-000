3aca550f4c5491c4c45f9f81ab69ed50b1c8b877
github username <github email address>
2018-02-14 20:43:22 -0500

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,19 @@
-# Write your #display_rainbow method here
+require "pry"
+
+#def display_rainbow(colors)
+#  new_colors = []
+#  colors.each {|color|
+#    new_colors.push("#{color[0].upcase}: #{color}")
+#  }
+#  puts new_colors.join(", ")
+#end
+
+def display_rainbow(colors)
+  print "R: #{colors[0]}, "
+  print "O: #{colors[1]}, "
+  print "Y: #{colors[2]}, "
+  print "G: #{colors[3]}, "
+  print "B: #{colors[4]}, "
+  print "I: #{colors[5]}, "
+  print "V: #{colors[6]}\n"
+end
