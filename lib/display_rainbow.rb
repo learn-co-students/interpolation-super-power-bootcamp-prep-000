5c3ebd4c9d879550bc83494db69b180a521b9ce1
Chris Dundon <chris.dundon@gmail.com>
2018-03-16 22:53:39 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,8 @@
 # Write your #display_rainbow method here
+def display_rainbow(arr)
+  first = ["R", "O", "Y", "G", "B", "I", "V"]
+  
+  out = "#{first[0]}: #{arr[0]}, #{first[1]}: #{arr[1]}, #{first[2]}: #{arr[2]}, #{first[3]}: #{arr[3]}, #{first[4]}: #{arr[4]}, #{first[5]}: #{arr[5]}, #{first[6]}: #{arr[6]}"
+  
+  puts(out)
+end  
