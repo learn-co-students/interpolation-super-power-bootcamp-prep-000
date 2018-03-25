e3733d29e422012b332a68cac7ecf4c9c03fdba0
Yong-Nicholas Kim <yongnicholaskim@gmail.com>
2018-03-25 18:32:32 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,11 @@
 # Write your #display_rainbow method here
+
+def display_rainbow(colors)
+  output = ""
+  
+  colors.each do | color |
+    output += "#{color[0].upcase}: #{color},"
+  end
+  
+  puts output
+end
