518d3c8ca74efab83cbaad585bedb2a73cf5e44b
Dalton Pierce <dnnp2011@gmail.com>
2018-03-29 04:48:15 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,19 @@
-# Write your #display_rainbow method here
+# def display_rainbow(colors)
+#   rainbow = String.new;
+#   for color in colors do #loop through colors
+#     puts "Loop";
+#     unless color == colors[-1] #If this is not the last element
+#       rainbow.concat(" #{color.slice(0).upcase}: #{color},");
+#     else
+#       rainbow.concat(" #{color.slice(0).upcase}: #{color}");
+#     end
+#   end
+#   rainbow.slice!(0); #Removes the space at the front
+#   puts rainbow;
+# end
+
+# display_rainbow(["red", "green", "blue"]);
+
+def display_rainbow(colors)
+  puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}";
+end
