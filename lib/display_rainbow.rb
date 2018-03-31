660c52bea1b122abe4cb4bf3c7c5b85d5aac6e74
Melissa Real <melissa.real@hotmail.com>
2018-03-31 18:54:57 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,4 @@
 # Write your #display_rainbow method here
+def display_rainbow(rcolors)
+  puts "R: #{rcolors[0]}, O: #{rcolors[1]}, Y: #{rcolors[2]}, G: #{rcolors[3]}, B: #{rcolors[4]}, I: #{rcolors[5]}, V: #{rcolors[6]}"
+end
