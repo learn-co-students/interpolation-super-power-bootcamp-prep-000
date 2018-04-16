9b836b179963b641056fbb4e48cfb8dfd1e0a49f
jessebillard <jessebillard@mac.com>
2018-04-16 14:34:14 -0400

    Interpolation super power

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,16 @@
-# Write your #display_rainbow method here
+#colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+
+#def display_rainbow_with_each(array)
+#   upcase_array = []
+#   array.each do |x|
+#       upcase_array << "#{x[0].upcase}: #{x}"
+#   end
+#   puts upcase_array.join(", ")
+#end
+
+#display_rainbow_with_each(colors)
+
+def display_rainbow(array)
+    puts "R: #{array[0]}, O: #{array[1]}, Y: #{array[2]}, G: #{array[3]}, B: #{array[4]}, I: #{array[5]}, V: #{array[6]}"
+end
+
