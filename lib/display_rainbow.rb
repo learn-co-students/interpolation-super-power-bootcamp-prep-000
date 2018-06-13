103c5f9fdddcf8a2dfa99d2d5cd512fd21085597
Danny O'B <dannyk85.dob@gmail.com>
2018-06-13 10:43:32 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,22 @@
 # Write your #display_rainbow method here
+
+# much better to learn about iterators, neater code
+# array iterator construct => array.each { |x| puts x }
+
+#def display_rainbow(array_colours)
+#  array_colours.sort()
+#  array_colours.each{
+#    |colour| 
+#    print "#{colour[0].upcase}: #{colour}"
+#  }
+#end
+
+def display_rainbow(array_colours)
+  print "R: #{array_colours[0]}, "
+  print "O: #{array_colours[1]}, "
+  print "Y: #{array_colours[2]}, "
+  print "G: #{array_colours[3]}, "
+  print "B: #{array_colours[4]}, "
+  print "I: #{array_colours[5]}, "
+  print "V: #{array_colours[6]}\n"
+end
