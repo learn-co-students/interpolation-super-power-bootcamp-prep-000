7ae093de9e99174ad600b0c4aea4598fba5e1035
wkdewey@gmail.com <wkdewey@gmail.com>
2018-06-29 20:35:00 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,11 @@
 # Write your #display_rainbow method here
+def display_rainbow(colors)
+  rainbow = ""
+  colors.length.times do |i|
+    rainbow += "#{colors[i][0].upcase}: #{colors[i]}"
+    if i < colors.length - 1
+      rainbow += ", "
+    end
+  end
+  puts rainbow
+end
