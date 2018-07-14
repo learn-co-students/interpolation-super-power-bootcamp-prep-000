3fda769c5e57a2ea04f1809a60e7215a3c3ef1f9
Lung Chen <lungheichen@gmail.com>
2018-07-14 14:35:00 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,6 @@
 # Write your #display_rainbow method here
+
+def display_rainbow(arr)
+  l = ["R", "O", "Y", "G", "B", "I", "V"]
+  puts "#{l[0]}: #{arr[0]}, #{l[1]}: #{arr[1]}, #{l[2]}: #{arr[2]}, #{l[3]}: #{arr[3]}, #{l[4]}: #{arr[4]}, #{l[5]}: #{arr[5]}, #{l[6]}: #{arr[6]}"
+end
