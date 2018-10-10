3afbf7acdd9aaf21a07b5171b1c94adce9ed6d1d
Mary Selig <seligmar@gmail.com>
2018-10-10 20:10:31 +0000

    Done.

---
+++ learn.rb
@@ -0,0 +1,6 @@
+def alphabet(letters)
+  letters = ["b", "a", "c"]
+  puts "The first letter in the alphabet is #{letters[1]."
+  puts "The second letter in the alphabet is #{letters[0]." 
+  puts "The third letter in the alphabet is #{letters[2]."
+end
