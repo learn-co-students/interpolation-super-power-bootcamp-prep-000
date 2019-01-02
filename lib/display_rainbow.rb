7235f49de78acd9dc267985ce14957e8194fb186
PRATHAMESH PRABHUDESAI <prathamesh1993@gmail.com>
2019-01-02 06:53:42 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,4 @@
 # Write your #display_rainbow method here
+def display_rainbow(color)
+  puts color[0][0].upcase + ": " + color[0] + ", " + color[1][0].upcase + ": " + color[1] + ", " + color[2][0].upcase + ": " + color[2] + ", " + color[3][0].upcase + ": " + color[3] + ", " + color[4][0].upcase + ": " + color[4] + ", " + + color[5][0].upcase + ": " + color[5] + ", " + color[6][0].upcase + ": " + color[6] 
+end
