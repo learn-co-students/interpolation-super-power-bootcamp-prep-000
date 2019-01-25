cc3938b04341530a2853da5cae22a8da11cbedc9
Andreas P. Gortsilas <apgortsilas@aggate.gr>
2019-01-25 18:50:31 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -6,6 +6,7 @@ def display_rainbow(arrColors)
   #   strRet = "#{strRet}#{c.chars.first.upcase}: #{c}#{', ' if i < arrColors.length-1}"
   # end
   # puts strRet
+
   puts "R: #{arrColors[0]}, O: #{arrColors[1]}, Y: #{arrColors[2]}, G: #{arrColors[3]}, B: #{arrColors[4]}, I: #{arrColors[5]}, V: #{arrColors[6]}"
 end
 
