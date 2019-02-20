b7af48df7b1f5850bd833934b3fc4a9b5db7e9e7
Mike Doig <mike.j.doig@gmail.com>
2019-02-20 16:27:42 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,4 @@
 # Write your #display_rainbow method here
+def display_rainbow(colors)
+  puts "#{colors[0].chars.first.upcase}: #{colors[0]}, #{colors[1].chars.first.upcase}: #{colors[1]}, #{colors[2].chars.first.upcase}: #{colors[2]}, #{colors[3].chars.first.upcase}: #{colors[3]}, #{colors[4].chars.first.upcase}: #{colors[4]}, #{colors[5].chars.first.upcase}: #{colors[5]}, #{colors[6].chars.first.upcase}: #{colors[6]}"
+end
