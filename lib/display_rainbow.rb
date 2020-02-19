094925778d3ea35fe5f3244e802003192f3fb650
Andrew Dalmedo <andrewdalmedo@gmail.com>
2020-02-19 08:05:25 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,7 @@
 # Write your #display_rainbow method here
+
+# default colors: red, orange, yellow, green, blue, indigo, violet
+def display_rainbow(col)
+  print "R: #{col[0]}, O: #{col[1]}, Y: #{col[2]}, G: #{col[3]}, "
+  puts  "B: #{col[4]}, I: #{col[5]}, V: #{col[6]}"
+end
