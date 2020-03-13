379b3009d586a1b935f6969ca4105ce1e2bc61e9
impactlearner <time.darden@gmail.com>
2020-03-13 19:14:05 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,16 @@
 # Write your #display_rainbow method here
+#'red' = R: red, 'orange' = O: orange, 'yellow' = Y: yellow, 'green' = G: green, 'blue' = B: blue, 'indigo' = I: indigo, 'violet' = V: violet
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+
+def display_rainbow(colors)
+  puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
+
+end
+
+# def print_business_card(person)
+#   puts
+#   "Name: #{person[0]},
+#   Age: #{person[1]},
+#   Occupation: #{person[2]}"
+#   puts "Contact: #{person[3]}"
+# end
