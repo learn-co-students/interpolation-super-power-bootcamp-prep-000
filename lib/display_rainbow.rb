3263ccdec7fe26bf8b8999b396bec2317216c5b7
Sarina Ocal <sarinaocal@gmail.com>
2020-06-13 19:30:19 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -8,8 +8,10 @@ def display_rainbow(colors)
     I: Indigo;
     V: Violet"
 
-puts "#{red[0]}; #{orange[1]}; #{yellow[2]}; #{green[3]}; #{blue[4]; #{indigo[5]}; #{violet[6]}"
- 
+puts "#{red[0]}; #{orange[1]}; #{yellow[2]}; #{green[3]}; #{blue[4]; #{indigo[5]}; #{violet[6]}
+
+
+
 end
 
   display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
