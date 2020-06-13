5c3203555307ca25047d4e25e15616ce62b87aab
Sarina Ocal <sarinaocal@gmail.com>
2020-06-13 01:27:54 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,17 +1,13 @@
-# Write your #display_rainbow method here
-def display_rainbow(colors);
-
-R = "red"
-O = "orange"
-Y = "yellow"
-G = "green"
-B = "blue"
-I = "indigo"
-V = "violet"
-
-
-
-  puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
-end
-
-display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
+def display_rainbow(colors)
+  
+  colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+  
+  puts "The first letter in the alphabet is: #{colors[0]}"
+  
+  puts "The second letter in the alphabet is: #
+  
+  puts "The third letter in the alphabet is: #{colors[1]}"
+  
+  puts "The fourth letter in the alphabet is 
+  
+  display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
--- s.rb
+++ s.rb
@@ -1,3 +1,7 @@
+three = 2 + l
 name = "Bob"
 
-puts "Hello, #{name}"
+puts "1 plus 1 is #{1 + 1} and 1 plus 2 is #{three} and 2 times 2 is #{2 * 2}. Oh, hey #{name}!"
+
+
+# => "1 plus 1 is 2 and 1 plus 2 is 3 and 2 times 2 is 4. Oh, hey Bob!"
