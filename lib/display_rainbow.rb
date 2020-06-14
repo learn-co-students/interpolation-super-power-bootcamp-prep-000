bee5bb3c938647c6a7f7655db30f57650ba54528
Sarina Ocal <sarinaocal@gmail.com>
2020-06-14 01:24:18 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,17 +1,14 @@
-def display_rainbow(colors)
+def display_rainbow(colors) #definre a method that takes in a single arguemnt the arguements name is colors colors is a variable representation of an array
 
-   "R: Red;
-    O: Orange;
-    Y: Yellow;
-    G: Green;
-    B: Blue;
-    I: Indigo;
-    V: Violet"
 
-puts "#{red[0]}; #{orange[1]}; #{yellow[2]}; #{green[3]}; #{blue[4]; #{indigo[5]}; #{violet[6]}
+
+puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
+
+#We want to interpolate each of the items in the array into the string.
+
+
+
 
 
 
 end
-
-  display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
